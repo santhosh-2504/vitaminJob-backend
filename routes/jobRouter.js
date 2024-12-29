9927cc68a6e0b4c40e754feb1a328d38a0891625
santhosh-2504 <santhoshanantha58@gmail.com>
2024-12-30 02:59:47 +0530

    Removed Authorization for Applying Jobs 1

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -3,7 +3,6 @@ import {
     getAllJobs, 
     getASingleJob, 
     getAllBookmarkedJobs,
-    applyForJob
 } from "../controllers/jobController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
@@ -11,7 +10,6 @@ const router = express.Router();
 router.get("/getall", getAllJobs);
 router.get("/get/:id", getASingleJob);
 router.get("/bookmarked-jobs", isAuthenticated, getAllBookmarkedJobs);
-router.post("/apply/:id",  isAuthenticated, applyForJob);
 
 
 export default router;
