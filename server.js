48cad8eabe29e8e3295ae1cb0539be97a4e3bf66
santhosh-2504 <santhoshanantha58@gmail.com>
2024-12-30 00:26:55 +0530

    App error fix

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import app from "./app.js"
 const PORT = process.env.PORT || 10000; // Use the port from the environment or fallback to 10000 for local dev
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
