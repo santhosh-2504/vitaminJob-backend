9db36fbf2ad7d1ee2b29147ee62094f669abd3ca
santhosh-2504 <santhoshanantha58@gmail.com>
2024-12-31 20:47:28 +0530

    Auth Error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,19 +3,18 @@ import { catchAsyncErrors } from "./catchAsyncErrors.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken"
 
-export const isAuthenticated = catchAsyncErrors(async(req, res, next)=>{
+export const isAuthenticated = catchAsyncErrors(async(req, res, next) => {
     const {token} = req.cookies;
+    
     if(!token){
-        return next(new ErrorHandler("User is not autheticated", 400));
+        return next(new ErrorHandler("User is not authenticated", 400));
     }
+    
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.user = await User.findById(decoded.id);
         next();
-      } catch (error) {
+    } catch (error) {
         return next(new ErrorHandler("Invalid or expired token", 401));
-      }
-    req.user =  await User.findById(decoded.id);
-
-    next();
-})
+    }
+});
